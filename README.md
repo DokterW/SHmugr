481b4ea4bb86be42be971eda8c0215f802a97add
Dr. Waldijk <DokterW@users.noreply.github.com>
2017-04-06 22:00:04 +0200

    SoLazy

---
+++ README.md
@@ -0,0 +1,7 @@
+# SHmugr
+
+If you are lazy like I am and tend to switch between runlevel 3 and 5, this is the perfect script for you!
+
+Just run it and then reboot.
+
+Really, there is not more too it.
---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# SHmugr v0.1
+# Made by Dr. Waldijk
+# A simple script to switch between runlevel 3 & 5.
+# Read the README.md for more info, but you will find more info here below.
+# By running this script you agree to the license terms.
+# -----------------------------------------------------------------------------------
+SHMUGR=$(systemctl get-default)
+if [ $SHMUGR = "graphical.target" ]; then
+    systemctl set-default multi-user.target
+elif [ $SHMUGR = "multi-user.target" ]; then
+    systemctl set-default graphical.target
+fi
+systemctl get-default
